cd993bda9989b1d2d7432591e6b35d7fe4ba5212
Vadim <dolinenko2006@ya.ru>
2023-01-07 20:04:12 +0300

    finish but there is problem with posts.json

--- loader/views.py
+++ loader/views.py
@@ -3,9 +3,16 @@ from loader.utils import save_picture
 from main.utils import PostHandler
 import logging
 
-logging.basicConfig(filename='basic.log', level=logging.INFO)
 
 
+logger= logging.getLogger()
+logger.setLevel(logging.DEBUG) # or whatever
+handler = logging.FileHandler('basic.log', 'w', 'utf-8') # or whatever
+formatter = logging.Formatter('%(name)s %(message)s') # or whatever
+handler.setFormatter(formatter) # Pass handler as a parameter, not assign
+logger.addHandler(handler)
+#handler = logging.FileHandler('basic.log', 'w', encoding='utf-8')  # так не работает тоже
+#logging.basicConfig(filename='basic.log', level=logging.INFO, encoding='utf-8') # так не работает тоже
 loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates')
 
 @loader_blueprint.route("/post")
